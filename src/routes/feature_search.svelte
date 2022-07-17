ebe1a7d11bd6bd5518d1097ed190c7e38da3832f
lvigerust <l.vigerust@gmail.com>
2022-07-17 18:06:28 +0200

    init

---
+++ src/routes/feature_search.svelte
@@ -0,0 +1,35 @@
+<div class="content">
+	<div class="search">
+		<div class="form-control">
+			<input type="text" placeholder="Search movie" class="input input-bordered " />
+		</div>
+		<button class="btn btn-ghost btn-circle">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="h-5 w-5"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke="currentColor"
+				><path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+				/></svg
+			>
+		</button>
+	</div>
+</div>
+
+<style>
+	.content {
+		height: 80vh;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+	.search {
+		display: flex;
+		flex-direction: row;
+	}
+</style>
